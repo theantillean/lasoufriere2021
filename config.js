9be7f9b8bccb822341dbcd58544d397eaefcc849
theantillean <45528193+theantillean@users.noreply.github.com>
2021-04-18 17:07:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const config = {
   description:
     "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
   sideBarInfo: ["Name"],
-  popupInfo: ["Name", "Address", "Phone", "Occupancy"],
+  popupInfor: ["Name", "Address", "Phone", "Occupancy"],
   filters: [
     
     {
